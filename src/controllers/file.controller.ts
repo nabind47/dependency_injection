062a7ac88d2ca7befe87f91ff687e59c74cd5f50
nabind47 <nabindhami619@gmail.com>
2024-01-17 22:21:01 +0545

    feat: conflict jindagee

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
-import { s3Uploader } from "../utils/s3-uploader";
+
+import { clUploader } from "../utils/cloudinary";
 
 export const fileController = {
   upload: async (req: Request, res: Response) => {
     // Upload file to S3
 
-    await s3Uploader.upload("test.mp4");
+    // await s3Uploader.upload("test.mp4");
+    await clUploader.upload("test.mp4");
     res.json({ message: "Hello World!!" });
   },
 };
---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,8 @@
+export const clUploader = {
+  upload: async (filename: string) => {
+    // upload to clx1
+
+    console.log(`Uploading ${filename} to cl...`);
+    return true;
+  },
+};
