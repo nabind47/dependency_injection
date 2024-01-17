606b56c6da2270247546d729c51f360360f89579
nabind47 <nabindhami619@gmail.com>
2024-01-17 22:49:05 +0545

    feat: dynamically

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import express from "express";
 import { fileController } from "./controllers/file.controller";
 import { clUploader } from "./utils/cloudinary";
+import { s3Uploader } from "./utils/s3-uploader";
 
 const app = express();
 
-app.get("/", (req, res) =>
-  fileController.upload(req, res, { uploader: clUploader })
-);
+app.get("/", (req, res) => {
+  const uploaderStr = req.query.uploader;
+
+  if (uploaderStr === "s3") {
+    fileController.upload(req, res, { uploader: s3Uploader });
+    return;
+  } else if (uploaderStr === "cloudinary") {
+    fileController.upload(req, res, { uploader: clUploader });
+    return;
+  } else {
+    res.status(400).json({ message: "Invalid uploader" });
+    return;
+  }
+});
 
 export default app;
